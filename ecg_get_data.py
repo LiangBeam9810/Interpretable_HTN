981af583f16b62457fcef53985e9dd8a1c6d05b1
LiangBeam9810 <41311565+LiangBeam9810@users.noreply.github.com>
2022-09-04 21:22:40 +0800

    k fold validation and earlystopping
    1. add the k fold validation, each fold will save the best model named
    2.add the Early stopping, save the check point model

--- ecg_get_data.py
+++ ecg_get_data.py
@@ -1,10 +1,12 @@
 from random import sample
+from wsgiref import validate
 import numpy as np
 import os
 from tqdm import tqdm
 import torch
 import torch.utils.data as Data
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 
 
@@ -34,7 +36,36 @@ def MAX_MIN_normalization_by_feactures(x,feature_rangetuple=(-1,1) ):
         x_normalized[i] = (min_max_scaler.fit_transform(data_swap)).swapaxes(0,1)  # turn shape back to (changnal,timesteps)
     return x_normalized
 
+def get_k_fold_dataset(fold,x,y,k = 5):
+    if(fold<=0):#防止fold过小
+        fold = 1
+    x = MAX_MIN_normalization_by_feactures(x)
+    x = torch.FloatTensor(x)  #turn numpy to tensor
+    y = torch.LongTensor(y)
 
+    data_len_for_each_fold_nums = int(len(y)/k)
+    validate_end_index = int(data_len_for_each_fold_nums*fold)
+
+    if(validate_end_index >len(y)): #防止超长度
+        validate_end_index = len(y)
+    elif(validate_end_index<data_len_for_each_fold_nums):#防止超长度
+        validate_end_index = data_len_for_each_fold_nums
+    validate_start_index = int(validate_end_index - data_len_for_each_fold_nums)
+    
+    INDEX_LIST = torch.arange(0,len(y))
+    validate_INDEX_LIST = torch.arange(validate_start_index,validate_end_index) # 左闭右开[validate_start_index,validate_end_index)
+    validate_MARK_INDEX = torch.isin(INDEX_LIST,validate_INDEX_LIST) # 把validate对应的INDEX位置用Ture标志出来
+                                                                  # train对应的位置就是False
+    validate_x = x[validate_MARK_INDEX]
+    validate_y = y[validate_MARK_INDEX]
+    train_x = x[~validate_MARK_INDEX]
+    train_y = y[~validate_MARK_INDEX]
+    print(('*'*50+"The %2d fold" % fold)+'*'*50)
+    validate_end_index = int(data_len_for_each_fold_nums*fold)
+    print("validate form %5d to %5d , all nums is %5d" % (validate_start_index,validate_end_index,len(validate_y)))
+    validate_dataset = Data.TensorDataset(validate_x, validate_y)
+    train_dataset = Data.TensorDataset(train_x, train_y)
+    return train_dataset,validate_dataset
 
 
 
@@ -53,10 +84,3 @@ def load_numpy_dataset_to_tensor_dataset(x,y,random_seed,train_rate = 0.8):
     print("train_dataset: %d  %d" % ( (train_dataset[:][1]).sum(),train_dataset.__len__()-(valid_dataset[:][1]).sum() ) )
     return  train_dataset,valid_dataset
 
-def load_small_numpy_dataset_to_tensor_dataset(x,y,random_seed):
-    #x = MAX_MIN_normalization_by_feactures(x)
-    torch.manual_seed(random_seed) 
-    x = torch.FloatTensor(x)  #turn numpy to tensor
-    y = torch.LongTensor(y)
-    dataset = Data.TensorDataset(x, y)
-    return dataset
--- train_test_validat.py
+++ train_test_validat.py
@@ -44,13 +44,52 @@ def test_model(test_loader,criterion,model,device):
         loss = criterion(outputs,labels)
 
         #print("output:",outputs)
-        print("labels:",labels)
+        #print("labels:",labels)
         outputs_ = outputs.clone()
         _,pred = outputs_.max(1) # 求概率最大值对应的标签
-        print("pred:",pred)
+        #print("pred:",pred)
         num_correct = (pred == labels).sum().item()
         acc = num_correct/len(labels)
         test_loss.append(loss.item())
         test_acc.append(acc)
 
     return np.mean(test_loss),np.mean(test_acc)
+
+class EarlyStopping:
+    
+    def __init__(self,patience=7, verbose=True,model_path = "./",delta = 0):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.model_path = model_path
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}\n')
+            if self.counter >= self.patience:
+                self.early_stop = True
+                self.save_checkpoint(val_loss, model)
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+        return self.early_stop
+
+    def save_checkpoint(self, val_loss, model):
+
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+            print(" "*20+'-'*50+'\n')
+        torch.save(model, self.model_path+'/EarlyStoping.pt')                 # 这里会存储迄今最优的模型
+        self.val_loss_min = val_loss
